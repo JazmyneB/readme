0b482f2866ac9ba3679d595c2f3cead1ea44b5e6
JazmyneB <bradleyj1@email.arizona.edu>
2021-06-26 18:45:23 -0700

    typos on package & param issue

--- potential-enigma-main/Develop/index.js
+++ potential-enigma-main/Develop/index.js
@@ -1,6 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
-const fs = reuire("fs");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = ["What is your Project Title?", "Please include a Description of your Project:", "Would you like to include Table of Contents? (Y/N)", "What are the steps required to install your project?", "Provide Instructions and Examples for use", "Are there any Collaborators? (Y/N)"];
@@ -36,7 +36,7 @@ const promptUser = () => {
         {
             type: "confirm",
             name: "confirmTOC",
-            message: "Would you like to include a Table Of Contents section? (Y/N)",
+            message: "Would you like to include a Table Of Contents section?",
             default: true
         },
         {
@@ -51,15 +51,69 @@ const promptUser = () => {
                     return false;
                 }
             }
+        },
+        {
+            type: "input",
+            name: "Usage",
+            message: "Please provide instructions and examples for use:",
+            validate: (usageInput) => {
+                if (usageInput){
+                    return true;
+                } else{
+                    console.log("Please provide instructions for Use!")
+                    return false;
+                }
+            }
+
+
+        },
+        {
+            type: 'confirm',
+            name: 'confirmCollab',
+            message: "Are there any collaborators?",
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'Credits',
+            message: "List your collaborators with the Links to their Github profiles:",
+            when: ({confirmCollab}) => {
+                if (confirmCollab){
+                    return true;
+                } else {
+                    return false;
+                }
+            }        
+        },
+        {
+            type: "confirm",
+            name: "confirmL",
+            message: "Would you like to include a license?",
+            default: true
+
+        },
+        {
+            type: 'list',
+            name: 'License',
+            message: "Which License would you like to include?",
+            choices: ["MIT", "Apache", "GPL"],
+            when: ({confirmL}) => {
+                if (confirmL){
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ])
 }
+promptUser().then(answers => console.log(answers));
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
